598567b0f53f1e66d8813c5a31e8bac2aedfe2f2
Samin Alnajafi <saminalnajafi96@gmail.com>
2016-12-08 08:58:55 +0000

    Homepage v3.1

---
+++ AndroidStudioProjects/Homepagev3/app/src/main/java/com/example/android/homepagev3/Skills.java
@@ -0,0 +1,26 @@
+package com.example.android.homepagev3;
+
+/**
+ * Created by Samin on 07/12/2016.
+ */
+
+public class Skills
+{
+    private String skillName = "";
+
+    public Skills(String skillName)
+    {
+        super();
+        this.skillName = skillName;
+    }
+
+    public String getSkillName()
+    {
+        return skillName;
+    }
+
+    public void setSkillName()
+    {
+        this.skillName = skillName;
+    }
+}
